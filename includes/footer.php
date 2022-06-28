e214a75524741991a759f6980c7ffc91a1fa2e3a
Shashank Upadhyaya <76894348+shaxhxnk@users.noreply.github.com>
2022-06-28 21:50:04 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+
+<head>
+</head>
+
+<body>
+
+<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center" style="padding:50px;">
+      Copyright &copy;REMS
+    </div>
+
+    
+  </footer>
+  </html>
